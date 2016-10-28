de0e75eda4d837c615c2338ce3c4b54153adf128
Nathan Poirier <nathan@poirier.io>
2016-10-28 23:02:56 +0200

    Add google analitycs tracking code

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -91,5 +91,17 @@
                     ]) !!};
 </script>
 <script src="{{ rev_asset('assets/js/app.js') }}"></script>
+
+<!-- Production start -->
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+    ga('create', 'UA-86429797-1', 'auto');
+    ga('send', 'pageview');
+</script>
+<!-- Production end -->
+
 </body>
 </html>
