0fa530e7a48d0af8ac5747d17040c7438311e164
Nathan Poirier <nathan@poirier.io>
2018-07-13 17:20:41 +0200

    Remove old versions with DB versions seeder (+ rename 1.7.10 to 1.7)

--- database/seeds/MinecraftVersionsSeeder.php
+++ database/seeds/MinecraftVersionsSeeder.php
@@ -6,8 +6,8 @@ class MinecraftVersionsSeeder extends Seeder
 {
     private $versionsByType = [
         'PC' => [
-            5   => '1.7.10',
-            47  => '1.8',
+            5 => '1.7', // 1.7.10 (not 1.7.2)
+            47 => '1.8',
             107 => '1.9',
             210 => '1.10',
             315 => '1.11',
@@ -24,26 +24,22 @@ class MinecraftVersionsSeeder extends Seeder
      */
     public function run()
     {
+        $ids = [];
         foreach ($this->versionsByType as $type => $versions) {
             foreach ($versions as $protocol_id => $name) {
-                $exists = DB::table('versions')->where('type', $type)->where('name', $name)->exists();
-                if (!$exists) {
-                    try {
-                        DB::table('versions')->insert([
-                            'type'        => $type,
-                            'protocol_id' => $protocol_id,
-                            'name'        => $name,
-                        ]);
-                    } catch (\Illuminate\Database\QueryException $e) {
-                        /*
-                         * Ignore duplicate entry exception
-                         */
-                        if ($e->getCode() != 23000 /* ER_DUP_CODE */) {
-                            throw $e;
-                        }
-                    }
+                $id = DB::table('versions')->select('id')->where('type', $type)->where('name', $name)->first();
+                if ($id !== null) {
+                    $id = $id->id;
+                } else {
+                    $id = DB::table('versions')->insertGetId([
+                        'type' => $type,
+                        'protocol_id' => $protocol_id,
+                        'name' => $name,
+                    ]);
                 }
+                $ids[] = $id;
             }
         }
+        DB::table('versions')->whereNotIn('id', $ids)->delete();
     }
 }
